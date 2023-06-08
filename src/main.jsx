d869a106786198fe296c55a1f44c6f6a68800620
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-06-08 14:46:12 +0900

    Create Welcome page

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Start from "./Start";
+import "./Asset/Asset.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Start />
+  </React.StrictMode>
+);
