23df4159bf71afbf5d1b58fecfb0ffd19dabdbd9
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-06-17 11:17:49 +0900

    insert images

---
+++ public/js/Style.js
@@ -0,0 +1,27 @@
+{
+}
+
+{
+  const togglePassword = document.querySelector("#toggle_password");
+  const togglePasswordConf = document.querySelector("#toggle_password_conf");
+  const password = document.querySelector("#text_password");
+  const confirmPass = document.querySelector("#confirm_password");
+
+  togglePassword.addEventListener("click", function (e) {
+    // toggle the type attribute
+    const type =
+      password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    // toggle the eye slash icon
+    this.classList.toggle("fa-eye");
+  });
+
+  togglePasswordConf.addEventListener("click", function (e) {
+    // toggle the type attribute
+    const type =
+      confirmPass.getAttribute("type") === "password" ? "text" : "password";
+    confirmPass.setAttribute("type", type);
+    // toggle the eye slash icon
+    this.classList.toggle("fa-eye");
+  });
+}
