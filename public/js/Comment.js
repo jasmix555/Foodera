4c6d9594eee05e052020c34ce1c4a5f2166e3031
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-06-20 16:32:16 +0900

    create comment function

--- public/js/Comment.js
+++ public/js/Comment.js
@@ -1,5 +1,42 @@
-{
-  const input = document.querySelector("#comment");
-  const submit = document.querySelector("#submit");
-  console.log(input, submit);
+const input = document.querySelector("#comment");
+const submit = document.querySelector("#submit");
+console.log(input, submit);
+const textWrapper = document.querySelector(".post-info-text");
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (input.value == 0) {
+  }
+  //else, take the inputted value of the text area and send it(or let a addtext variable work)
+  else {
+    addText();
+    input.value = "";
+  }
+});
+
+input.addEventListener("keypress", function (event) {
+  //when enter is pressed or also when the there is no text in the textarea
+  if (event.code === "Enter" && !input.value == "") {
+    //prevent a bug where it opens a whole new website instead of staying in the same page
+    //take the textarea value and enter it to the list
+    addText();
+    //after taking the value, erase the past value so that is looks clean
+    input.value = "";
+  } else if (event.code === "Enter" && input.value == "") {
+    //when there is no text inputted, alert that there needs to be a text
+    event.preventDefault();
+  }
+});
+
+function addText() {
+  textWrapper.innerHTML += `
+  <div class="post-comment-wrapper">
+    <div class="post-text-prof">
+      <a href="#" style="background-image: url(../public/img/people/0.jpeg);"> </a>
+      </div>
+      <p class="post-comment">${input.value}</p>
+  </div>
+  `;
 }
