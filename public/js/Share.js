841abe3c10dcf5d292c963391532f7019f4aae1a
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-06-27 16:13:42 +0900

    finishing share function

--- public/js/Share.js
+++ public/js/Share.js
@@ -4,16 +4,16 @@ const xMark = document.querySelector(".fa-xmark");
 
 shareBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    shareWrapper.style.display = "block";
+    shareWrapper.style.transform = "translateY(0)";
   });
 });
 
 xMark.addEventListener("click", (e) => {
-  shareWrapper.style.display = "none";
+  shareWrapper.style.transform = "translateY(100%)";
 });
 
 shareWrapper.addEventListener("click", (e) => {
   if (e.target === shareWrapper) {
-    shareWrapper.style.display = "none";
+    shareWrapper.style.transform = "translateY(100%)";
   }
 });
