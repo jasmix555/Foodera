459f5c12eff355e2cdcaaca0a18b5cb64ce380d3
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-07-06 14:35:49 +0900

    create location header

--- public/js/Counter.js
+++ public/js/Counter.js
@@ -17,3 +17,12 @@ heartButtons.forEach((button) => {
     }
   });
 });
+
+{
+  const button = document.querySelector(".location-main button");
+  const icon = document.querySelector(".fa-chevron-down");
+
+  button.addEventListener("click", function () {
+    icon.classList.toggle("rotated");
+  });
+}
