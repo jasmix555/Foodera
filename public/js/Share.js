6d895cab05bf18f4465c41ca67bef69dd583f188
jasmix555 <94930720+jasmix555@users.noreply.github.com>
2023-07-11 14:40:27 +0900

    popup

--- public/js/Share.js
+++ public/js/Share.js
@@ -17,3 +17,27 @@ shareWrapper.addEventListener("click", (e) => {
     shareWrapper.style.transform = "translateY(100%)";
   }
 });
+
+const bookmarkBtn = document.querySelectorAll(".fa-bookmark");
+const bookmarkWrap = document.querySelector(".bookmark-save-wrapper");
+const bookmarkClose = document.querySelector(".save-close");
+
+bookmarkBtn.forEach((btn) => {
+  bookmarkBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (btn.classList.contains("fa-solid")) {
+        bookmarkWrap.style.transform = "translateY(0)";
+      }
+    });
+  });
+});
+
+bookmarkClose.addEventListener("click", (e) => {
+  bookmarkWrap.style.transform = "translateY(100%)";
+});
+
+bookmarkWrap.addEventListener("click", (e) => {
+  if (e.target === bookmarkWrap) {
+    bookmarkWrap.style.transform = "translateY(100%)";
+  }
+});
